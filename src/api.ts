eeb9f7b07dbf7ad2403191affa901b86ee23057f
r1chwo0n <woonzy09@gmail.com>
2025-07-02 18:49:33 +0700

    bug: can't read env file

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
-// src/lib/api.ts
 import axios from 'axios'
 
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3003'
+console.log("BASE_URL =", import.meta.env.VITE_API_URL);
 
 export const api = axios.create({
   baseURL,
